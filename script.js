e21493524f9d8f175239e167f3535a51e2117af5
Devanshu Chatterjee <devanshu4190@gmail.com>
2021-07-01 03:48:45 +0530

    fine tune, add hover effect, create contact page

---
+++ script.js
@@ -0,0 +1,9 @@
+// window.onscroll = function()
+// {
+//     var v = document.getElementById('navbar');
+//     v.classList.toggle('sticky', window.scrollY > 0);
+// }
+
+particlesJS.load('particles-js', 'particlesjs-config.json');
+
+
